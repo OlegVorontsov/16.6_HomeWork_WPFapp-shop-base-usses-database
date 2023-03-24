c09831386440397356810051c6a9e165d7f98bdf
Олег Воронцов <oleg-voroncov@mail.ru>
2023-03-24 16:32:23 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _16._6_HomeWork_WPFapp_shop_base_usses_database
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿create table ClientsInfo
+(
+	id int identity (1,1) not null,
+	surname nvarchar (20) not null,
+	[name] nvarchar (10) not null,
+	patronymic nvarchar (20) not null,
+	phonenumber float,
+	email nvarchar (30) not null
+)
+
+insert into ClientsInfo ([surname],[name],[patronymic],[phonenumber],[email]) values (
+						'Ivanov', 'Ivan', 'Ivanovich', '9261454312', 'ivanivanov@mail.ru')
+						
+insert into ClientsInfo ([surname],[name],[patronymic],[phonenumber],[email]) values (
+						'Petrov', 'Petr', 'Petrovich', '9295428946', 'petrpetrov@yandex.ru')
+
+insert into ClientsInfo ([surname],[name],[patronymic],[phonenumber],[email]) values (
+						'Sergeev', 'Sergey', 'Sergeevich', '9266542398', 'sergeysergeev@google.com')
